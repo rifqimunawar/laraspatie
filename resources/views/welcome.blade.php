90266b563eeebe6855e2b62e390859e560aa6d3e
Rifqi Munawar <102643244+rifqimunawar@users.noreply.github.com>
2023-05-03 22:52:54 +0700

    update tittle

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -6,7 +6,7 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Laravel</title>
+        <title>ini title</title>
 
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
